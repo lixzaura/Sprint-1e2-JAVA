67976b07a785e76183d007efd86ac90c823e5cee
paulo-henrique253 <paulo253hgg@gmail.com>
2026-05-03 18:36:33 -0300

    feat: MissaoViagensBicicleta
    Missão que consiste em realizar 3 viagens de bicicleta para completar

--- SoulMove/src/br/com/soulmove/missao/MissaoViagensBicicleta.java
+++ SoulMove/src/br/com/soulmove/missao/MissaoViagensBicicleta.java
@@ -2,9 +2,22 @@ package br.com.soulmove.missao;
 
 import br.com.soulmove.usuario.Usuario;
 
-public class MissaoReducaoCarbono extends Missao implements Cumprivel{
+public class MissaoViagensBicicleta extends Missao implements Cumprivel{
+
+    public MissaoViagensBicicleta(){
+        pontos = 30;
+        titulo = "Faça 3 viagens de bicicleta";
+        desc = "Faça 3 viagens de bicicleta para promover a sustentabilidade e resgatar os pontos 😊";
+    }
+
+
     @Override
     public boolean verificarCumprida(Usuario user) {
-        if (/*checar disponibilidade*/)
+        if (/*checar disponibilidade &&*/ user.getViagensBicicleta() > 3)
+            return true;
+
+
+        return false;
+
     }
 }
