59f8913254b51dc76f2d1953fbeb1d8d84b2f928
Julia de Freitas Barbosa <139178072+lixzaura@users.noreply.github.com>
2026-05-09 13:18:36 -0300

    Criar menu da queridíssima SoulMove
    Menu visual da SoulMove, com caracteres mágicos mas sem nem um método ainda :(

--- SoulMove/src/br/com/soulmove/SoulMove.java
+++ SoulMove/src/br/com/soulmove/SoulMove.java
@@ -1,21 +1,4 @@
-import java.util.Scanner;
+package br.com.soulmove;
 
-void main() {
-
-    Scanner leitura = new Scanner(System.in);
-
-    int escolha = 0;
-
-    while (escolha != 0) {
-        System.out.println("""
-                             Escolha uma das opções abaixo
-                                1. Cadastrar Usuário.
-                                2. Calcular emissão.
-                                3. Converter pontos.
-                                0. Sair do programa.
-                                """);
-
-        escolha = leitura.nextInt();
-
-    }
+public class SoulMove {
 }
